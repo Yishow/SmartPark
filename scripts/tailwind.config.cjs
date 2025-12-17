3a0ad160e1ce7b9a0a2f99b24af25607d1f9deb5
Yishow <yishow@gmail.com>
2025-12-17 15:40:50 +0800

    chore(style): integrate Tailwind via PostCSS, remove CDN

---
+++ scripts/tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './index.html',
+    './index.tsx',
+    './**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
